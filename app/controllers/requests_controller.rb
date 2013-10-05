c6e6e3fa075f26dddd105fc6722f31986e4ad5c2
Ivan Dymkov <mstrdymio@gmail.com>
2013-10-06 03:33:47 +0800

    Tab current

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,9 +2,11 @@ class RequestsController < ApplicationController
   before_filter :authenticate_user!, :collect_workers
 
   def index
+    @active_tab = "currents"
   end
 
   def history
+    @active_tab = "history"
   end
 
   def print
